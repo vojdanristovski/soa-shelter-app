5f5f1cce6c4676928023d1fa817e050d4fa0b077
Katerina Gligorovska <katerina.gligorovska@nebula.com>
2022-05-15 17:53:23 +0200

    move to async db

--- src/database.py
+++ src/database.py
@@ -1,12 +1,9 @@
 from typing import Generator
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker,scoped_session
 
-engine=create_engine("postgresql://shelter-app:shelter-app@db:5432/shelter-app")
+from src.settings import Settings, get_settings
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
 
-ScopedSession=scoped_session(sessionmaker(bind=engine))
-
-
-
-def get_session() -> Generator[scoped_session, None, None]:
-    yield ScopedSession()
+settings: Settings = get_settings()
+engine = create_async_engine(settings.database_url, future=True, echo=True)
+ScopedSession = sessionmaker(bind=engine, class_=AsyncSession)
