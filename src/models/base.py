56eb80496d883b910e2cf46a371223835ddbe8b1
Katerina Gligorovska <katerina.gligorovska@nebula.com>
2022-05-15 17:52:28 +0200

    extract common model logic

---
+++ src/models/base.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, DateTime
+from sqlalchemy.orm import declarative_mixin, declared_attr, declarative_base
+from sqlalchemy.sql import func
+
+Base = declarative_base()
+
+
+@declarative_mixin
+class BaseDbModel:
+    id_ = Column("id", Integer, primary_key=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+
+    @declared_attr
+    def __tablename__(cls):
+        return f"{cls.__name__.lower()}s"
--- src/models/dog.py
+++ src/models/dog.py
@@ -1,24 +1,15 @@
-from sqlalchemy import String,Boolean,Integer,Column
-from ..enums import DogBreed,DogStatus
+from sqlalchemy import String, Boolean, Integer, Column
+from ..enums import DogBreed, DogStatus
 from sqlalchemy.dialects.postgresql import ENUM
-from src.models import Base
+from src.models.base import BaseDbModel, Base
 
-class Dog(Base):
-    __tablename__="dogs"
-    id=Column(Integer,primary_key=True)
-    image_url=Column(String(255),nullable=True)
-    name=Column(String(255),nullable=False)
-    is_chipped=Column(Boolean,default=False)
-    coordinates=Column(String(255),nullable=True)
-    breed=Column(
-        ENUM(DogBreed, name="breeds"),
-        name="breed",
-        nullable=False
-        )
-    dog_status=Column(
-        ENUM(DogStatus, name="statuses"),
-        name="dog_status",
-        nullable=False
+
+class Dog(BaseDbModel, Base):
+    image_url = Column(String(255), nullable=True)
+    name = Column(String(255), nullable=False)
+    is_chipped = Column(Boolean, default=False)
+    coordinates = Column(String(255), nullable=True)
+    breed = Column(ENUM(DogBreed, name="breeds"), name="breed", nullable=False)
+    dog_status = Column(
+        ENUM(DogStatus, name="statuses"), name="dog_status", nullable=False
     )
-    
-    
